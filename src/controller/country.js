2afe7f6512eddbab7aaeaade5413228bf99c3959
brookmezgu <brookmezgebu64@gmail.com>
2020-01-02 16:38:36 +0300

    progress: add logic for the controllers

--- src/controller/country.js
+++ src/controller/country.js
@@ -4,16 +4,74 @@ import axios from 'axios'
 /**
  * Handles get requests made to the country endpoint.
  * normally the country name would be in the query section 
- * @example '/country?q=ethiopia'
+ * @example '/country?country=ethiopia'
  * @param {express.req} req 
  * @param {express.res} res 
  */
 export function handleCountryGet(req , res) {
-    res.send({ "data" : "will be here."})
+    let country = req.query.country;
+    let returnableJSON = {};
+    
+    if (!country)
+        return res.status(406).send({
+            status: 406,
+            error: "country query is required.",
+            data: {}
+        })
+
+    axios.get(`http://country.io/${country}`).then(returned => {
+        let c = cheerio.load(returned.data);
+        let tds = c('td');
+
+        for (var i = 0; i < tds.length; i = i+3) {
+            returnableJSON[`${tds[i].children[0].children[0].data}`] = tds[i+1].children[0].data
+        }
+        
+        res.send({
+            status: 200,
+            error: {},
+            data: returnableJSON
+        })
+    }).catch(err => {
+        res.status(err.response.status).send({
+            status: err.response.status,
+            error: err.response.data,
+            data: {}
+        })
+    })
 }
 
 export function handleCountryPost(req , res) {
+    let country = req.body.country;
+    let returnableJSON = {};
     
+    if (!country)
+        return res.status(406).send({
+            status: 406,
+            error: `country query is required.`,
+            data: {}
+        })
+
+    axios.get(`http://country.io/${country}`).then(returned => {
+        let c = cheerio.load(returned.data);
+        let tds = c('td');
+
+        for (var i = 0; i < tds.length; i = i+3) {
+            returnableJSON[`${tds[i].children[0].children[0].data}`] = tds[i+1].children[0].data
+        }
+        
+        res.send({
+            status: 200,
+            error: {},
+            data: returnableJSON
+        })
+    }).catch(err => {
+        res.status(err.response.status).send({
+            status: err.response.status,
+            error: err.response.data,
+            data: {}
+        })
+    })
 }
 
 export function handleCountryDelete(req , res) {

505f9f30c44d9d987dc291b5feefefd471b03dae
brookmezgu <brookmezgebu64@gmail.com>
2020-01-27 23:42:51 +0300

    progress: add the type def of Country

--- src/graphql/server.js
+++ src/graphql/server.js
@@ -3,6 +3,43 @@ const { ApolloServer , gql } = require('apollo-server')
 const typeDefs = gql`
     type Country {
         country_name: String
+        dialing_code: String
+        iso2: String
+        iso3: String
+        capital: String
+        main_lang: String
+
+        currency: String
+        gdp: String
+        population: String
+
+        # GEO ---
+        continent: String
+        location: String
+        land: String
+        climate: String
+        national_hazzards: String
+        note: String
+        terrain: String
+
+        # DEMO --- 
+        life_exp: String
+        median_age: String
+        birth_date: String
+        death_rate: String
+        sex_ratio: String
+        literacy: String
+
+        # TRANSPORTATION ---
+        roadways: String
+        railways: String
+        airports: String
+        waterways: String
+        heliports: String
+        airports_paved: String
+
+        # ECONOMY
+        gdp_per_capita: String
     }
 
     type Query {
